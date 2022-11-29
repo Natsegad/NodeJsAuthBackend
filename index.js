0341d492132d8c91a373cd3459e9a0758378e462
Saaler05 <67513194+UnknowXi@users.noreply.github.com>
2022-11-30 00:15:17 +0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import {authRouters} from "./routers/auth.js"
 const app = express()
 app.use(express.json())
 
-mongoose.connect("mongodb+srv://saaler:13134777r@cluster0.kmzerty.mongodb.net/?retryWrites=true&w=majority").then(()=>{
+mongoose.connect("mongodb+srv://saer:1377r@cluster0.kmzerty.mongodb.net/?retryWrites=true&w=majority").then(()=>{
     console.log("Db connected")
 }).catch((err)=>{
     console.log("Db not connected ",err)
